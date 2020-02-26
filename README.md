c11d757b49ce91b8ab42026f61caa0feae08725e
xxshambm <ext.michael.shamberger@tieto.com>
2020-02-26 19:15:29 +0200

    add info about missing countries

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 wget --no-parent -r http://download.geonames.org/export/zip/
 
+# missing some EU countries #
+
+Starting to find potential sources of the postal codes
+
+gr - greece
+https://github.com/MentatInnovations/grpostcodes
+https://github.com/MentatInnovations/grpostcodes/blob/master/data/postcode_lat_long_output_file.csv
+Apache 2.0 license
+
+ba - Bosnia
+
+
+al - albania
+https://en.wikipedia.org/wiki/Postal_codes_in_Albania
+(I contacted them for official source)
+
+me - montenegro
+
+
+cy - cyprus
+http://www.posta.gov.ct.tr/Portals/117/KKTC%20Posta%20Kodlar%C4%B1.pdf
--- missing.sh
+++ missing.sh
@@ -2,7 +2,7 @@
 # download all zip codes from geonames and then make then lower case and remove non europe
 
 # Declare an array of string with type
-declare -a countryisolist=("UA" "FR" "ES" "SE" "DE" "FI" "NO" "PL" "IT" "GB" "RO" "BY" "GR" "BG" "IS" "PT" "CZ" "DK" "HU" "RS" "AT" "IE" "LT" "LV" "HR" "BA" "SK" "EE" "NL" "CH" "MD" "BE" "AL" "MK" "SI" "ME" "CY" "LU" "FO" "AD" "MT" "LI" "GG" "SM" "GI" "MC" "VA")
+declare -a countryisolist=("UA" "FR" "ES" "SE" "DE" "FI" "NO" "PL" "IT" "GB" "RO" "BY" "GR" "BG" "IS" "PT" "CZ" "DK" "HU" "RS" "AT" "IE" "LT" "LV" "HR" "BA" "SK" "EE" "NL" "CH" "MD" "BE" "AL" "MK" "SI" "ME" "CY" "LU" "FO" "AD" "MT" "LI" "GG" "SM" "MC" "VA")
 
 mkdir tmp
 # Iterate the string array using for loop
